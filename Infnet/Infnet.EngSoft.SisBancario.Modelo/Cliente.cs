a7b2a3ec9d7bb125fa3117800efb03ba278af5cf
William Rego <wregobv@gmail.com>
2012-10-05 14:27:16 -0300

    Primeiro Release

---
+++ Infnet/Infnet.EngSoft.SisBancario.Modelo/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infnet.EngSoft.SistBancario.Modelo
+{
+    public abstract class Cliente
+    {
+        public string Nome { get; set; }
+        public List<Endereco> Enderecos { get; set; }
+        public List<Telefone> Telefones { get; set; }
+    }
+}
---
+++ Infnet/Infnet.EngSoft.SisBancario.Modelo/Fisica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infnet.EngSoft.SistBancario.Modelo
+{
+    class Fisica : Cliente
+    {
+        public decimal Renda { get; set; }
+        public int CPF { get; set; }
+
+    }
+}
